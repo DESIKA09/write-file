e984b37a333beeab5aa87eb5865d655ce8a2d420
DESIKA09 <110881693+DESIKA09@users.noreply.github.com>
2022-08-17 12:38:45 +0530

    Add files via upload

---
+++ large num.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+a=int(input ('enter a number1='))
+
+
+# In[4]:
+
+
+b=int(input ('enter a number1='))
+
+
+# In[7]:
+
+
+if a>b:
+    print('The larger number is={}'.format(a))
+else:
+    print('The larger number is={}'.format(b))
+
+
+# In[ ]:
+
+
+
+
---
+++ small num.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=int(input ('enter a number1='))
+
+
+# In[2]:
+
+
+b=int(input ('enter a number1='))
+
+
+# In[3]:
+
+
+if a<b:
+    print('The smaller number is={}'.format(a))
+else:
+    print('The smaller number is={}'.format(b))
+
+
+# In[ ]:
+
+
+
+
